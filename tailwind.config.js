56d8e6a527e8e2c1410bad4490bab939186a6d1e
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-07-16 00:09:08 -0700

    feat: Add custom colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-white":"#fff",
+        "color-primary":"#01051e",
+        "color-primary-light":"#020726",
+        "color-primary-dark":"#010417",
+        "color-secondary":"#ff7d3b",
+        "color-gray":"#333",
+        "color-blob":"#A427DF"
+      }
+    },
   },
   plugins: [],
 }
