77f1dbba1d18f182d3baac3a24de031b65d3449b
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-03 15:54:25 +0530

    fix: Make navbar sticky and visible even after scroll.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const navbar = document.querySelector('header');
+window.onscroll = () => {
+  if(window.scrollY > 100){
+    navbar.classList.add('bg-color-primary-dark');
+    navbar.classList.add('border-b');
+    navbar.classList.add('border-color-gray');
+  }else{
+    navbar.classList.remove('bg-color-primary-dark');
+    navbar.classList.remove('border-b');
+    navbar.classList.remove('border-color-gray');
+  }
+}
+
 //Samll Screen Menu
 
 const hamburger = document.querySelector("#hamburger");
